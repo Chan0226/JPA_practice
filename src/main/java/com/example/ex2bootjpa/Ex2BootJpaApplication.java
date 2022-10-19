355d839f015956643ca0211d8f8e92fd74ab751c
chan <3233978@naver.com>
2022-10-19 23:28:03 +0900

    first commit

---
+++ src/main/java/com/example/ex2bootjpa/Ex2BootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex2bootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex2BootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex2BootJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex2bootjpa/Ex2BootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex2bootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2BootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
